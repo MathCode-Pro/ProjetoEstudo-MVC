70e3b6361e24bf001598e4e3a02e094870477515
Matheus Oliveira <matheus_o._santos@outlook.com>
2020-06-25 23:18:38 -0300

    Adicionando detalhes dos Sellers

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -56,5 +56,18 @@ namespace SalesWebMvc.Controllers
             _sellersService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Seller obj = _sellersService.FindById(id.Value);
+
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
     }
 }
