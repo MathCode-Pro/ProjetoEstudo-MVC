124ce89edd9d5ffa19fc8c6f8352ac8698aece0c
Matheus Oliveira <matheus_o._santos@outlook.com>
2020-06-26 20:54:32 -0300

    Garantindo integridade referencial da aplicacao com tratamento de excessao personalizado para a funcao Delete

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -36,8 +36,15 @@ namespace SalesWebMvc.Services
         public async Task RemoveAsync(int id)
         {
             var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+
+            try
+            {
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }catch(DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales!");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
